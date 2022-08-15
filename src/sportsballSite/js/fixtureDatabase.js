eaa965de743ef486ab13b284baba5cd781aa105d
Anthony Dixon <adixon88@sky.com>
2022-08-15 10:29:47 +0100

    finished sportsball website

--- src/sportsballSite/js/fixtureDatabase.js
+++ src/sportsballSite/js/fixtureDatabase.js
@@ -1,3 +1,4 @@
+const homePageTrigger = document.getElementById('fixturesLinkHomePage');
 const textSection = document.getElementById('fixtureSection');
 const augustBtn = document.getElementById('augustBtn');
 const septemberBtn = document.getElementById('septemberBtn');
@@ -14,35 +15,34 @@ function matchdayResults(year, month, date, day, versus, ground, referee, goalSF
     this.goalSFC = goalSFC;
     this.goalOther = goalOther;  
 }
-
 const matchDay1 = new matchdayResults (
     2022, 'August', 6, 'Saturday',
-    'Morpeth Town', 'Thunderdome', 'Ross Geller',
-    ['Bing', 'Tribbiani'], ['Green']
+    'Tinsel Town FC', 'Thunderdome', 'Ross Geller',
+    ['Bing 34', 'Tribbiani 45+1'], ['Cruise 87']
 );
 const matchDay2 = new matchdayResults (
     2022, 'August', 20, 'Saturday',
-    'Scarborough AFC', 'The Flamingo Land Stadium', 'Frank-N-Furter',
-    ['Bing'], ['Geller', 'Buffay', 'Gunther']
+    'Old York Utd', 'Central Perk Stadium', 'Frank-N-Furter',
+    ['Bing 22'], ['Geller 31', 'Buffay 67', 'Gunther 76']
 )
 const matchDay3 = new matchdayResults (
     2022, 'September', 10, 'Saturday',
-    'Brackley', 'St James Park', 'Ross Geller',
-    ['Bing', 'Tribbiani', 'Bing'], []
+    'Riverside', 'St James Park', 'Ross Geller',
+    ['Bing 19', 'Tribbiani 85', 'Bing 90+3'], []
 );
 const matchDay4 = new matchdayResults (
     2022, 'September', 27, 'Saturday',
-    'Kidderminster', 'Thunderdome', 'Joan of Arc',
-    ['Tribbiani', 'Tribbiani'], ['Jack', 'July']
+    'United Utd', 'Thunderdome', 'Joan of Arc',
+    ['Tribbiani 65', 'Tribbiani 68'], ['Jack 32', 'Judy 48']
 )
 
-// Filters
-var augustResults = [matchDay1, matchDay2];
-var septemberResults = [matchDay3, matchDay4];
+// Variables
 var seasonResults = [matchDay1, matchDay2, matchDay3, matchDay4];
-
+var scorerPara = document.createElement('p'),
+clone;
 var matches = -1;
 
+// Filters
 function monthFilterAugust() {
     for (let i = 0; i < seasonResults.length; i++) {
         matches++;
@@ -78,52 +78,104 @@ function displayResults () {
 
     if (seasonResults[matches].ground === 'Thunderdome') {
         var homeTeam = `Sportsball FC`;
+        var homeTeamClass = `sportsballFC`;
         var homeTeamScore = `${seasonResults[matches].goalSFC.length}`;
-        var homeTeamScorers = `${seasonResults[matches].goalSFC}`
-        var homeTeamScorers = `${seasonResults[matches].goalSFC}`;
+        var homeTeamScorers = document.createElement('div');
+        homeTeamScorers.classList.add('teamScorers', 'homeTeamScorers')
+        seasonResults[matches].goalSFC.forEach(function (scorer, index) {
+            clone = scorerPara.cloneNode();
+            clone.textContent = scorer;
+            clone.classList.add(`scorer${index}`, 'scorer')
+            homeTeamScorers.appendChild(clone);
+        })
         var awayTeam = `${seasonResults[matches].versus}`;
+        var awayTeamClass = awayTeam.toLowerCase();
+        awayTeamClass = awayTeamClass.replace(/\s+/g, '');
         var awayTeamScore = `${seasonResults[matches].goalOther.length}`;
-        var awayTeamScorers = `${seasonResults[matches].goalOther}`;
+        var awayTeamScorers = document.createElement('div');
+        awayTeamScorers.classList.add('teamScorers', 'awayTeamScorers')
+        seasonResults[matches].goalOther.forEach(function (scorer, index) {
+            clone = scorerPara.cloneNode();
+            clone.textContent = scorer;
+            clone.classList.add(`scorer${index}`, 'scorer')
+            awayTeamScorers.appendChild(clone);
+        })
     } else {
         var homeTeam = `${seasonResults[matches].versus}`;
+        var homeTeamClass = homeTeam.toLowerCase();
+        homeTeamClass = homeTeamClass.replace(/\s+/g, '');
         var homeTeamScore = `${seasonResults[matches].goalOther.length}`;
-        var homeTeamScorers = `${seasonResults[matches].goalOther}`;
+        var homeTeamScorers = document.createElement('div');
+        homeTeamScorers.classList.add('teamScorers', 'homeTeamScorers')
+        seasonResults[matches].goalOther.forEach(function (scorer, index) {
+            clone = scorerPara.cloneNode();
+            clone.textContent = scorer;
+            clone.classList.add(`scorer${index}`, 'scorer')
+            homeTeamScorers.appendChild(clone);
+        })        
         var awayTeam = `Sportsball FC`;
+        var awayTeamClass = `sportsballFC`;
         var awayTeamScore = `${seasonResults[matches].goalSFC.length}`;
-        var awayTeamScorers = `${seasonResults[matches].goalSFC}`;
+        var awayTeamScorers = document.createElement('div');
+        awayTeamScorers.classList.add('teamScorers', 'awayTeamScorers')
+        seasonResults[matches].goalSFC.forEach(function (scorer, index) {
+            clone = scorerPara.cloneNode();
+            clone.textContent = scorer;
+            clone.classList.add(`scorer${index}`, 'scorer')
+            awayTeamScorers.appendChild(clone);
+        })
     }
 
-    let gameInfoPara = document.createElement('p');
+    let resultContainer = document.createElement('div');
+    let gameInfoDiv = document.createElement('div');
+    let gameInfoGround = document.createElement('p');
+    let gameInfoDate = document.createElement('p');
+    let homeTeamDiv = document.createElement('div');
     let homeTeamPara = document.createElement('p');
+    let homeTeamImg = document.createElement('div');
+    let awayTeamDiv = document.createElement('div');
     let awayTeamPara = document.createElement('p');
+    let awayTeamImg = document.createElement('div');
     let homeTeamScorePara = document.createElement('p');
     let awayTeamScorePara = document.createElement('p');
-    let homeTeamScorersPara = document.createElement('p');
-    let awayTeamScorersPara = document.createElement('p');
+    let teamsFlex = document.createElement('div');
 
-    gameInfoPara.classList.add('gameInfo');
-    homeTeamPara.classList.add('teamName', 'homeTeam');
-    awayTeamPara.classList.add('teamName', 'awayTeam');
+    resultContainer.classList.add('resultContainer');
+    gameInfoDiv.classList.add('gameInfoFlex');
+    gameInfoGround.classList.add('gameInfoGround');
+    gameInfoDate.classList.add('gameInfoDate');
+    homeTeamDiv.classList.add('homeTeamContainer');
+    homeTeamImg.classList.add('teamImg', 'homeTeamImg', `${homeTeamClass}Img`);
+    homeTeamPara.classList.add('teamName', 'homeTeam', `${homeTeamClass}`);
+    awayTeamDiv.classList.add('awayTeamContainer');
+    awayTeamImg.classList.add('teamImg', 'awayTeamImg', `${awayTeamClass}Img`);
+    awayTeamPara.classList.add('teamName', 'awayTeam', `${awayTeamClass}`);
     homeTeamScorePara.classList.add('teamScore', 'homeTeam');
     awayTeamScorePara.classList.add('teamScore', 'awayTeam');
-    homeTeamScorersPara.classList.add('teamScorers', 'homeTeam');
-    awayTeamScorersPara.classList.add('teamScorers', 'awayTeam');
+    teamsFlex.classList.add('teamsFlex');
 
-    gameInfoPara.textContent = `${ground} ${date}`;
+    gameInfoGround.textContent = `${ground}`;
+    gameInfoDate.textContent = `${date}`;
     homeTeamPara.textContent = `${homeTeam}`;
     awayTeamPara.textContent = `${awayTeam}`;
     homeTeamScorePara.textContent = `${homeTeamScore}`;
     awayTeamScorePara.textContent = `${awayTeamScore}`;
-    homeTeamScorersPara.textContent = `${homeTeamScorers}`;
-    awayTeamScorersPara.textContent = `${awayTeamScorers}`;
 
-    textSection.appendChild(gameInfoPara);
-    textSection.appendChild(homeTeamPara);
-    textSection.appendChild(awayTeamPara);
-    textSection.appendChild(homeTeamScorePara);
-    textSection.appendChild(awayTeamScorePara);
-    textSection.appendChild(homeTeamScorersPara);
-    textSection.appendChild(awayTeamScorersPara);
+    gameInfoDiv.appendChild(gameInfoGround);
+    gameInfoDiv.appendChild(gameInfoDate);
+    homeTeamDiv.appendChild(homeTeamImg);
+    homeTeamDiv.appendChild(homeTeamPara);
+    homeTeamDiv.appendChild(homeTeamScorePara);
+    homeTeamDiv.appendChild(homeTeamScorers);
+    teamsFlex.appendChild(homeTeamDiv);
+    awayTeamDiv.appendChild(awayTeamImg);
+    awayTeamDiv.appendChild(awayTeamPara);
+    awayTeamDiv.appendChild(awayTeamScorePara);
+    awayTeamDiv.appendChild(awayTeamScorers);
+    teamsFlex.appendChild(awayTeamDiv);
+    resultContainer.appendChild(gameInfoDiv);
+    resultContainer.appendChild(teamsFlex);
+    textSection.appendChild(resultContainer);
 }
 
 function blankPage() {
@@ -133,3 +185,5 @@ function blankPage() {
         }
     }
 }
+
+monthFilterAugust();
