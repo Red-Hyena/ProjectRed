0e1c1b359fb3bcc52751062d5b9deede3d32aeef
Anthony Dixon <adixon88@sky.com>
2022-08-25 15:07:35 +0100

    about me page finished

--- src/portfolioMain/js/enterSiteScript.js
+++ src/portfolioMain/js/enterSiteScript.js
@@ -3,7 +3,6 @@ const introScreen = document.getElementById('introScreen');
 var introLines = document.querySelectorAll('.introLine');
 
 const navbar = document.querySelector('.navbar');
-const navAudio = new Audio('/src/portfolioMain/audio/mechanicalClamp.wav');
 
 const mainScreen = document.getElementById('mainScreen');
 
@@ -18,12 +17,39 @@ function animateScreenToggle() {
     })
     setTimeout(function(){
         introScreen.classList.replace('visible', 'hidden');
-        navbar.classList.replace('hidden', 'navVisible');
-        navbar.classList.add('moveFromLeft');
-        navAudioPlay();
+        navbar.classList.replace('hidden', 'navVisibleAlt');
+        navbar.classList.add('fadeIn');
+        setTimeout(function(){
+            mainScreen.classList.replace('hidden', 'visible');
+            mainScreen.classList.add('fadeIn');
+        }, 1500);
     }, 1500);
+
 }
 
-function navAudioPlay() {
-    navAudio.play();
-}
+// Video Play on Hover
+const video1 = document.querySelector('.video1');
+const video2 = document.querySelector('.video2');
+const video3 = document.querySelector('.video3');
+
+video1.addEventListener('mouseover', () => {
+    video1.play();
+})
+video1.addEventListener('mouseout', () => {
+    video1.currentTime = 0;
+    video1.pause();
+})
+video2.addEventListener('mouseover', () => {
+    video2.play();
+})
+video2.addEventListener('mouseout', () => {
+    video2.currentTime = 0;
+    video2.pause();
+})
+video3.addEventListener('mouseover', () => {
+    video3.play();
+})
+video3.addEventListener('mouseout', () => {
+    video3.currentTime = 0;
+    video3.pause();
+})
